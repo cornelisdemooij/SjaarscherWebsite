bea84fa37b659b89824f57ebd3ddd186918228db
Cornelis de Mooij <codemo@gmail.com>
2020-05-22 23:42:48 +0200

    Profile elements are now on top of each other, swiping makes them disappear

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Placeholder.
+window.addEventListener("profileSwipeEvent", (e) => {
+  const composedPath = e.composedPath();
+  if (composedPath && composedPath.length > 0) {
+    const elem = composedPath[0];
+    elem.parentNode.removeChild(elem);
+  }
+});
--- profileComponent.js
+++ profileComponent.js
@@ -16,7 +16,6 @@ class ProfileElement extends LitElement {
       }
       .drag-container-content {
         position: relative;
-        background-color: #FFFFFF;
         cursor: move;
         width: 100%;
         overflow: hidden;
@@ -48,17 +47,19 @@ class ProfileElement extends LitElement {
 
   static get properties() {
     return {
-      name: { type: String },
+      firstName: { type: String },
+      lastName: { type: String, value: '' },
+      age: { type: Number },
       oldMouseX: { type: Number },
       oldMouseY: { type: Number },
       oldElemX: { type: Number },
       oldElemY: { type: Number },
+      _swipeResult: { type: String },
     };
   }
 
   constructor() {
     super();
-    this.name = 'Ellie';
     this.oldMouseX = 0;
     this.oldMouseY = 0;
     this.oldElemX = 0;
@@ -72,11 +73,11 @@ class ProfileElement extends LitElement {
           <div class="image-container" @mousedown=${this._onMouseDown}>
             <img 
               class="profile-image"
-              src="assets/profilePictures/${this.name}/ellie.jpg" 
-              alt="${this.name}" 
-              title="${this.name}"
+              src="assets/profilePictures/${this.firstName}/${this.firstName.toLowerCase()}.jpg" 
+              alt="${this.firstName}" 
+              title="${this.firstName}"
             >
-            <div class="profile-name">Ellie van der Vlucht (19)</div>
+            <div class="profile-name">${this.firstName} ${this.lastName} ${this.age ? '(' + this.age + ')' : ''}</div>
           </div>
         </div>
       </div>
@@ -94,16 +95,17 @@ class ProfileElement extends LitElement {
     e = e || window.event;
     e.preventDefault();
 
-    const elem = this.shadowRoot.querySelectorAll('.drag-container-content')[0];
-    this.width = elem.offsetWidth;
-    this.height = elem.offsetHeight;
+    const dragContainer = this.shadowRoot.querySelectorAll('.drag-container')[0];
+
+    this.width = dragContainer.offsetWidth;
+    this.height = dragContainer.offsetHeight;
 
     // Get the mouse cursor position at startup:
     this.oldMouseX = e.clientX;
     this.oldMouseY = e.clientY;
     // Convert mouse cursor position to point on element: // TODO: Take into account existing rotation
-    this.oldElemX = e.clientX - parseInt(elem.offsetLeft);
-    this.oldElemY = e.clientY - parseInt(elem.offsetTop);
+    this.oldElemX = e.clientX - parseInt(dragContainer.offsetLeft);
+    this.oldElemY = e.clientY - parseInt(dragContainer.offsetTop);
     // Set handlers for mouse moving and button being released: // TODO: Add handlers for device touches.
     this.onmousemove = this._onMouseMove;
     this.onmouseup = this._onMouseUp;
@@ -123,8 +125,12 @@ class ProfileElement extends LitElement {
     const a = a1+a2;
     if (a > 0.5) {
       console.log('like');
+      this._swipeResult = 'like';
     } else if (a < -0.5) {
       console.log('dislike');
+      this._swipeResult = 'dislike';
+    } else {
+      this._swipeResult = 'none';
     }
     this.setRotation(a);
   }
@@ -134,6 +140,14 @@ class ProfileElement extends LitElement {
     // Stop moving the element when mouse button is released:
     this.onmousemove = null;
     this.onmouseup = null;
+    if (this._swipeResult !== 'none') {
+      const event = new CustomEvent('profileSwipeEvent', { 
+        detail: this._swipeResult,
+        bubbles: true, 
+        composed: true 
+      });
+      this.dispatchEvent(event);
+    }
   }
 }
 
